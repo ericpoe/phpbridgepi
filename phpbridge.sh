7aac4f32659baf52f15c84d50ff16f5ee6257ceb
Eric Poe <eric.poe@gmail.com>
2014-08-27 22:34:17 -0500

    Added some documentation to the installer.

--- phpbridge.sh
+++ phpbridge.sh
@@ -3,12 +3,12 @@
 
 sudo apt-get install aptitude tightvncserver gedit gedit-source-code-browser-plugin git apache2 php5 php5-cli php5-xdebug mysql-client mysql-server
 
+# Install PHP Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
+# Install and configure psysh
 composer g require psy/psysh:~0.1
-
-# echo alias psysh=\'~/.composer/vendor/bin/psysh\' >> ~/.bashrc
 
 echo <<-PSYSH_PATH
      # set PATH so it recognizes executables installed via Composer
