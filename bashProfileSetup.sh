6ed1754ba29d2521c8020053a9aff3701d6f6940
Eric Poe <eric.poe@gmail.com>
2014-09-01 11:17:05 -0500

    Reduced the number of VNC windows from 2 to 1.
    Just trying reduce the amount of resources.

--- bashProfileSetup.sh
+++ bashProfileSetup.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
-# Set up .bash_profile to work with PHPBridge Applications
+## Set up .bash_profile to work with PHPBridge Applications
 cat > ~/.bash_profile <<-BASH_PROFILE
 	if [ -f ~/.bashrc ]; then
 	   source ~/.bashrc
 	fi
 
-	# Automatically start VncServer
-	vncserver :0 -geometry 1920x1080 -depth 24 -dpi 96
-	vncserver :1
+	## Automatically start VncServer
+	vncserver :0
+	# If you want more than the default settings for VNC, try the following instead
+	# vncserver :0 -geometry 1920x1080 -depth 24 -dpi 96
 
-	# set PATH so it recognizes executables installed via Composer
+	## set PATH so it recognizes executables installed via Composer
 	if [ -d "$HOME/.composer/vendor/bin" ] ; then
 		PATH="$HOME/.composer/vendor/bin:$PATH"
 	fi
